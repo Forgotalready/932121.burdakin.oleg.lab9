d4371eb01d25ac06a8805d22e4317a3b8a2d637a
ForgotAlready <oleg.burdakin2016@yandex.ru>
2024-11-05 18:57:52 +0700

    3

--- scripts/fsm.js
+++ scripts/fsm.js
@@ -4,6 +4,7 @@ class FSM
 {
     constructor() 
     {
+        this.operationsStack = []
         this.states = Object.freeze({
             EMPTY : 0,
             NUM : 1,
@@ -13,7 +14,7 @@ class FSM
         this.numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
         this.inputElement = document.querySelector(".container__form__input")
 
-        this.state = this.states.EMPTY
+        this.operationsStack.push(this.states.EMPTY)
     }
 
     handleInput(input)
@@ -28,19 +29,32 @@ class FSM
         }
         else if(input === "=")
         {
+            const state = this.operationsStack.at(-1)
             this.inputElement.innerHTML = new Function('return ' + this.inputElement.textContent)().toString()
-            this.state = this.states.NUM
+            this.operationsStack = []
+            this.operationsStack.push(this.states.NUM)
         }
         else if(input === "C")
         {
+            const state = this.operationsStack.at(-1)
             this.inputElement.innerHTML = ""
-            this.state = this.states.EMPTY
+            this.operationsStack = []
+            this.operationsStack.push(this.states.EMPTY)
+        }
+        else if(input === "<-")
+        {
+            if(this.operationsStack.length != 1)
+            {
+                this.operationsStack.pop()
+                this.inputElement.textContent = this.inputElement.textContent.slice(0, -1)
+            }
         }
     }
 
     handleSign(input)
     {
-        switch(this.state)
+        const state = this.operationsStack.at(-1)
+        switch(state)
         {
             case this.states.SIGN:
                 break
@@ -50,12 +64,13 @@ class FSM
                 this.inputElement.innerHTML = `<span style="opacity: 0.3">${this.inputElement.innerHTML}</span>${input}`
                 break
         }
-        this.state = this.states.SIGN
+        this.operationsStack.push(this.states.SIGN)
     }
 
     handleNumber(input)
     {
-        switch(this.state)
+        const state = this.operationsStack.at(-1)
+        switch(state)
         {
             case this.states.NUM:
                 this.inputElement.innerHTML += input
@@ -64,6 +79,6 @@ class FSM
                 this.inputElement.innerHTML = `<span style="opacity: 0.3">${this.inputElement.innerHTML}</span>${input}`
                 break
         }
-        this.state = this.states.NUM
+        this.operationsStack.push(this.states.NUM)
     }
 }
